9e82cc97cb26e2c3d9ef502a04f3871596211ee3
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-10-04 19:33:46 +0530

    React Hooks & Context Api Project Setting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import './App.css'
-import ShopContextProvider from './context/ShopContext'
-import ShowProducts from './components/shopProducts'
-import Nav from './components/Nav'
-import ThemeContextProvider from './context/ThemeContext'
+
 const App = () => {
   return (
-    <div className='container mt-5'>
-      <ShopContextProvider>
-        <Nav/>
-        <ThemeContextProvider>
-        <ShowProducts />
-        </ThemeContextProvider>
-      </ShopContextProvider>
+    <div>
+      
     </div>
   )
 }
