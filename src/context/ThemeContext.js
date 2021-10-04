1ab181f988c747968e7c888751dc2b6cac906fc6
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-10-04 19:29:26 +0530

    add state file

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,9 +3,9 @@ import React,{createContext,useState} from 'react'
 export const ThemeContext = createContext()
 
 const ThemeContextProvider =(props)=>{
-     const color = useState({bgColor:"orange",textColor:"#333"})
+     const [color, setColor] = useState({bgColor:"orange",textColor:"#333"})
     return(
-        <ThemeContext.Provider value={{...color}}>
+        <ThemeContext.Provider value={{color}}>
 {props.children}
         </ThemeContext.Provider>
     )
