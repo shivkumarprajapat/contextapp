f8a328b60e5081a32d4a34b4fef6826de5423755
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-10-04 19:46:45 +0530

    Project Video 1

---
+++ src/components/ContactDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactDetails = () => {
+    return (
+        <div>
+            <h1>ContactDetails</h1>
+        </div>
+    )
+}
+
+export default ContactDetails
